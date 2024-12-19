81627f716d6a029de3fad6c761630cedc93ff6d8
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-19 19:43:07 +0530

    Understanding NoUniqueBeanDefinitionException in Spring

---
+++ example2/src/main/java/com/example/main/Example2.java
@@ -0,0 +1,14 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example2 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle vehicle1 = context.getBean(Vehicle.class);
+        System.out.println("vehicle name from spring bean: " + vehicle1.getName());
+    }
+}
