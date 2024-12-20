4955511833b10098c60b7769d8d6d2798d0e3cf9
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-21 01:06:57 +0530

    adding springboot

---
+++ example_19/src/test/java/com/cgnexus/example_19/Example19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgnexus.example_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
