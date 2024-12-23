e520a29d78a07d41c0b5ff4471dad12912a96795
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-23 13:26:12 +0530

    deny all the endpoints

--- cgnexusSchoolMainProject/src/main/java/com/cgnexus/example_19/config/SpringBootWebSecurityConfiguration.java
+++ cgnexusSchoolMainProject/src/main/java/com/cgnexus/example_19/config/SpringBootWebSecurityConfiguration.java
@@ -16,7 +16,7 @@ public class SpringBootWebSecurityConfiguration {
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                .anyRequest().permitAll()
+                .anyRequest().denyAll()
         );
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
