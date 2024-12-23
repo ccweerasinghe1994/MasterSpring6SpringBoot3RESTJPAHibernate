3c76c3a295909b5b8fb1b5d17bd621e7d60ba140
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-23 14:41:50 +0530

    security config

--- cgnexusSchoolMainProject/src/main/java/com/cgnexus/example_19/config/SpringBootWebSecurityConfiguration.java
+++ cgnexusSchoolMainProject/src/main/java/com/cgnexus/example_19/config/SpringBootWebSecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,19 +18,53 @@ public class SpringBootWebSecurityConfiguration {
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/home", "/").permitAll()
-                        .requestMatchers("/assets/**").permitAll()
-                        .requestMatchers("/courses").permitAll()
-                        .requestMatchers("/saveMsg").permitAll()
-                        .requestMatchers("/about").permitAll()
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/holidays/**").permitAll()
-                );
-        http.formLogin(withDefaults());
+
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/dashboard").authenticated()
+                .requestMatchers("/home", "/").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+        );
+
+        http.formLogin(loginConfig -> loginConfig
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
+                .failureUrl("/login?error=true")
+                .permitAll()
+        );
+
+        http.logout(logoutConfig -> logoutConfig
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .permitAll()
+        );
+
         http.httpBasic(withDefaults());
+
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
