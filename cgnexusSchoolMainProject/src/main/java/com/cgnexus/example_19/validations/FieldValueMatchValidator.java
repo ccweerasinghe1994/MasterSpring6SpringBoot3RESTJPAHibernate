169cf872bf5056fec8d7f357b8c7c899ea79bc2f
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-27 19:14:11 +0530

    adding custom validation

---
+++ cgnexusSchoolMainProject/src/main/java/com/cgnexus/example_19/validations/FieldValueMatchValidator.java
@@ -0,0 +1,55 @@
+package com.cgnexus.example_19.validations;
+
+import com.cgnexus.example_19.annotations.FieldsValueMatch;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class FieldValueMatchValidator implements ConstraintValidator<FieldsValueMatch, Object> {
+
+    private String field;
+    private String fieldMatch;
+
+    /**
+     * Initializes the validator in preparation for
+     * {@link #isValid(Object, ConstraintValidatorContext)} calls.
+     * The constraint annotation for a given constraint declaration
+     * is passed.
+     * <p>
+     * This method is guaranteed to be called before any use of this instance for
+     * validation.
+     * <p>
+     * The default implementation is a no-op.
+     *
+     * @param constraintAnnotation annotation instance for a given constraint declaration
+     */
+    @Override
+    public void initialize(FieldsValueMatch constraintAnnotation) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    /**
+     * Implements the validation logic.
+     * The state of {@code value} must not be altered.
+     * <p>
+     * This method can be accessed concurrently, thread-safety must be ensured
+     * by the implementation.
+     *
+     * @param value   object to validate
+     * @param context context in which the constraint is evaluated
+     * @return {@code false} if {@code value} does not pass the constraint
+     */
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        Object fieldValue = new BeanWrapperImpl(value)
+                .getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(value)
+                .getPropertyValue(fieldMatch);
+        if (fieldValue != null) {
+            return fieldValue.equals(fieldMatchValue);
+        } else {
+            return fieldMatchValue == null;
+        }
+    }
+}
